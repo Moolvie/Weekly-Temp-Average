2a3485461b948ae1c3ea74d334e3082df6d5b5e0
dougenos <dougenos@CMMOFFLINE>
2017-02-13 10:31:16 -0500

    Renamed Form1 to WeeklyTempAvg

---
+++ Weekly Temp Average/WeeklyTempAvg.Designer.vb
@@ -0,0 +1,246 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class WeeklyTempAvg
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TempOneInput = New System.Windows.Forms.TextBox()
+        Me.TempTwoInput = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TempThreeInput = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TempFourInput = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TempFiveInput = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.AverageTempOutput = New System.Windows.Forms.Label()
+        Me.CalcAvgButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.Status = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(36, 43)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(68, 16)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Week #1"
+        '
+        'TempOneInput
+        '
+        Me.TempOneInput.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TempOneInput.Location = New System.Drawing.Point(123, 40)
+        Me.TempOneInput.Name = "TempOneInput"
+        Me.TempOneInput.Size = New System.Drawing.Size(100, 22)
+        Me.TempOneInput.TabIndex = 1
+        '
+        'TempTwoInput
+        '
+        Me.TempTwoInput.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TempTwoInput.Location = New System.Drawing.Point(123, 68)
+        Me.TempTwoInput.Name = "TempTwoInput"
+        Me.TempTwoInput.Size = New System.Drawing.Size(100, 22)
+        Me.TempTwoInput.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(36, 71)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(68, 16)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Week #2"
+        '
+        'TempThreeInput
+        '
+        Me.TempThreeInput.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TempThreeInput.Location = New System.Drawing.Point(123, 96)
+        Me.TempThreeInput.Name = "TempThreeInput"
+        Me.TempThreeInput.Size = New System.Drawing.Size(100, 22)
+        Me.TempThreeInput.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(36, 99)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(68, 16)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Week #3"
+        '
+        'TempFourInput
+        '
+        Me.TempFourInput.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TempFourInput.Location = New System.Drawing.Point(123, 124)
+        Me.TempFourInput.Name = "TempFourInput"
+        Me.TempFourInput.Size = New System.Drawing.Size(100, 22)
+        Me.TempFourInput.TabIndex = 7
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(36, 127)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(68, 16)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Week #4"
+        '
+        'TempFiveInput
+        '
+        Me.TempFiveInput.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TempFiveInput.Location = New System.Drawing.Point(123, 152)
+        Me.TempFiveInput.Name = "TempFiveInput"
+        Me.TempFiveInput.Size = New System.Drawing.Size(100, 22)
+        Me.TempFiveInput.TabIndex = 9
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(36, 155)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(68, 16)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "Week #5"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(36, 190)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(67, 16)
+        Me.Label6.TabIndex = 10
+        Me.Label6.Text = "Average"
+        '
+        'AverageTempOutput
+        '
+        Me.AverageTempOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.AverageTempOutput.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.AverageTempOutput.Location = New System.Drawing.Point(123, 189)
+        Me.AverageTempOutput.Name = "AverageTempOutput"
+        Me.AverageTempOutput.Size = New System.Drawing.Size(100, 23)
+        Me.AverageTempOutput.TabIndex = 11
+        '
+        'CalcAvgButton
+        '
+        Me.CalcAvgButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CalcAvgButton.Location = New System.Drawing.Point(14, 249)
+        Me.CalcAvgButton.Name = "CalcAvgButton"
+        Me.CalcAvgButton.Size = New System.Drawing.Size(90, 30)
+        Me.CalcAvgButton.TabIndex = 12
+        Me.CalcAvgButton.Text = "Calculate"
+        Me.CalcAvgButton.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ClearButton.Location = New System.Drawing.Point(127, 249)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(90, 30)
+        Me.ClearButton.TabIndex = 13
+        Me.ClearButton.Text = "Cl&ear"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ExitButton.Location = New System.Drawing.Point(240, 249)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(90, 30)
+        Me.ExitButton.TabIndex = 14
+        Me.ExitButton.Text = "E&xit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.Status})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 291)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(342, 22)
+        Me.StatusStrip1.TabIndex = 15
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'Status
+        '
+        Me.Status.Name = "Status"
+        Me.Status.Size = New System.Drawing.Size(0, 17)
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(342, 313)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.CalcAvgButton)
+        Me.Controls.Add(Me.AverageTempOutput)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.TempFiveInput)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.TempFourInput)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TempThreeInput)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TempTwoInput)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TempOneInput)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Weekly Temperature Average"
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TempOneInput As System.Windows.Forms.TextBox
+    Friend WithEvents TempTwoInput As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TempThreeInput As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents TempFourInput As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents TempFiveInput As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents AverageTempOutput As System.Windows.Forms.Label
+    Friend WithEvents CalcAvgButton As System.Windows.Forms.Button
+    Friend WithEvents ClearButton As System.Windows.Forms.Button
+    Friend WithEvents ExitButton As System.Windows.Forms.Button
+    Friend WithEvents StatusStrip1 As System.Windows.Forms.StatusStrip
+    Friend WithEvents Status As System.Windows.Forms.ToolStripStatusLabel
+
+End Class
---
+++ Weekly Temp Average/WeeklyTempAvg.vb
@@ -0,0 +1,58 @@
+﻿Public Class WeeklyTempAvg
+
+
+    Private Sub CalcAvgButton_Click(sender As Object, e As EventArgs) Handles CalcAvgButton.Click
+        ' Declare the constant for averaging. we are averaging five weekly temperatures
+        Const NUMBER_OF_TEMPS As Integer = 5
+        Try
+            ' Declare the variables
+            Dim WeekTempOne As Single = 0.0
+            Dim WeekTempTwo As Single = 0.0
+            Dim WeekTempThree As Single = 0.0
+            Dim WeekTempFour As Single = 0.0
+            Dim WeekTempFive As Single = 0.0
+            Dim WeekAverageTemp As Single = 0.0
+
+            ' convert inputs to double
+            WeekTempOne = Convert.ToSingle(TempOneInput.Text)
+            WeekTempTwo = Convert.ToSingle(TempTwoInput.Text)
+            WeekTempThree = Convert.ToSingle(TempThreeInput.Text)
+            WeekTempFour = Convert.ToSingle(TempFourInput.Text)
+            WeekTempFive = Convert.ToSingle(TempFiveInput.Text)
+
+            ' calculate the average Temp
+            WeekAverageTemp = (WeekTempOne + WeekTempTwo + WeekTempThree + WeekTempFour + WeekTempFive) / NUMBER_OF_TEMPS
+
+            ' display the result
+            AverageTempOutput.Text = Convert.ToString(WeekAverageTemp)
+            Status.Text = String.Empty ' clear any error in the status bar
+        Catch
+            Status.Text = "You must enter numeric values for each week"
+        End Try
+    End Sub
+
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+        ' clear all the text boxes for another set of Weekly temps
+        TempOneInput.Clear()
+        TempTwoInput.Clear()
+        TempThreeInput.Clear()
+        TempFourInput.Clear()
+        TempFiveInput.Clear()
+        Status.Text = String.Empty ' clear any error in the status bar
+        AverageTempOutput.Text = String.Empty
+
+        ' set focus to the first Weekly Temp after clearing the form
+        TempOneInput.Focus()
+
+    End Sub
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        ' close the form
+        Me.Close()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' put the focus in the Week one tempurature text box when loading the form
+        TempOneInput.Focus()
+    End Sub
+End Class
