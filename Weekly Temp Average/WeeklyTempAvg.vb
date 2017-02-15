e625538149eb122dc22f6c9ddc3267723610762c
dougenos <dougenos@CMMOFFLINE>
2017-02-15 17:36:55 -0500

    Updated for more extensive error checking per programming challenge in chapter 5

--- Weekly Temp Average/WeeklyTempAvg.vb
+++ Weekly Temp Average/WeeklyTempAvg.vb
@@ -4,31 +4,56 @@
     Private Sub CalcAvgButton_Click(sender As Object, e As EventArgs) Handles CalcAvgButton.Click
         ' Declare the constant for averaging. we are averaging five weekly temperatures
         Const NUMBER_OF_TEMPS As Integer = 5
-        Try
-            ' Declare the variables
-            Dim WeekTempOne As Single = 0.0
-            Dim WeekTempTwo As Single = 0.0
-            Dim WeekTempThree As Single = 0.0
-            Dim WeekTempFour As Single = 0.0
-            Dim WeekTempFive As Single = 0.0
-            Dim WeekAverageTemp As Single = 0.0
 
-            ' convert inputs to double
-            WeekTempOne = Convert.ToSingle(TempOneInput.Text)
-            WeekTempTwo = Convert.ToSingle(TempTwoInput.Text)
-            WeekTempThree = Convert.ToSingle(TempThreeInput.Text)
-            WeekTempFour = Convert.ToSingle(TempFourInput.Text)
-            WeekTempFive = Convert.ToSingle(TempFiveInput.Text)
+        ' Declare the variables
+        Dim WeekTempOne As Single = 0.0
+        Dim WeekTempTwo As Single = 0.0
+        Dim WeekTempThree As Single = 0.0
+        Dim WeekTempFour As Single = 0.0
+        Dim WeekTempFive As Single = 0.0
+        Dim WeekAverageTemp As Single = 0.0
 
-            ' calculate the average Temp
-            WeekAverageTemp = (WeekTempOne + WeekTempTwo + WeekTempThree + WeekTempFour + WeekTempFive) / NUMBER_OF_TEMPS
-
-            ' display the result
-            AverageTempOutput.Text = Convert.ToString(WeekAverageTemp)
-            Status.Text = String.Empty ' clear any error in the status bar
-        Catch
-            Status.Text = "You must enter numeric values for each week"
-        End Try
+        ' convert inputs to double
+        If Single.TryParse(TempOneInput.Text, WeekTempOne) Then
+            If Single.TryParse(TempTwoInput.Text, WeekTempTwo) Then
+                If Single.TryParse(TempThreeInput.Text, WeekTempThree) Then
+                    If Single.TryParse(TempFourInput.Text, WeekTempFour) Then
+                        If Single.TryParse(TempFiveInput.Text, WeekTempFive) Then
+                            ' calculate the average Temp
+                            WeekAverageTemp = (WeekTempOne + WeekTempTwo + WeekTempThree + WeekTempFour + WeekTempFive) / NUMBER_OF_TEMPS
+                            ' display the result
+                            AverageTempOutput.Text = Convert.ToString(WeekAverageTemp)
+                            Status.Text = String.Empty ' clear any error in the status bar
+                        Else
+                            TempFiveInput.Focus()
+                            TempFiveInput.SelectionStart = 0
+                            TempFiveInput.SelectionLength = TempFiveInput.Text.Length
+                            Status.Text = "You must enter a numeric value in week #5"
+                        End If
+                    Else
+                        TempFourInput.Focus()
+                        TempFourInput.SelectionStart = 0
+                        TempFourInput.SelectionLength = TempFourInput.Text.Length
+                        Status.Text = "You must enter a numeric value in week #4"
+                    End If
+                Else
+                    TempThreeInput.Focus()
+                    TempThreeInput.SelectionStart = 0
+                    TempThreeInput.SelectionLength = TempThreeInput.Text.Length
+                    Status.Text = "You must enter a numeric value in week #3"
+                End If
+            Else
+                TempTwoInput.Focus()
+                TempTwoInput.SelectionStart = 0
+                TempTwoInput.SelectionLength = TempTwoInput.Text.Length
+                Status.Text = "You must enter a numeric value in Week #2"
+            End If
+        Else
+            TempOneInput.Focus()
+            TempOneInput.SelectionStart = 0
+            TempOneInput.SelectionLength = TempOneInput.Text.Length
+            Status.Text = "You must enter a numeric value in Week #1"
+        End If
     End Sub
 
     Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
